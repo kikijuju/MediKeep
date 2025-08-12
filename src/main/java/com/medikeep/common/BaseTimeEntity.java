c4c1536ae76b50e8874a6b1a4d1f6747f813e4ec
kikijuju <114811246+kikijuju@users.noreply.github.com>
2025-08-12 18:14:01 +0900

    Update BaseTimeEntity.java 주석 수정

--- src/main/java/com/medikeep/common/BaseTimeEntity.java
+++ src/main/java/com/medikeep/common/BaseTimeEntity.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // 이 클래스를 상속하는 엔티티들은 아래 필드들을 컬럼으로 인식하게 됩니다.
-@EntityListeners(AuditingEntityListener.class) // Auditing(자동 시간 관리) 기능을 활성화합니다.
+@MappedSuperclass // 이 클래스를 상속하는 엔티티들은 아래 필드들을 컬럼으로 인식
+@EntityListeners(AuditingEntityListener.class) // Auditing(자동 시간 관리) 기능 활성화
 public abstract class BaseTimeEntity {
 
-    @CreatedDate // 엔티티가 생성될 때 시간이 자동으로 저장됩니다.
+    @CreatedDate // 엔티티 생성시 시간 자동 저장
     private LocalDateTime createdAt;
 
-    @LastModifiedDate // 엔티티가 수정될 때 시간이 자동으로 저장됩니다.
+    @LastModifiedDate // 엔티티가 수정시 시간 자동 저장
     private LocalDateTime updatedAt;
 }
